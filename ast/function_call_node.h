b0f9fecaa71f0c9163ac8f68952314e5c529fc6d
Tomás Nunes <nunes.tomas@gmail.com>
2022-06-22 14:54:05 +0100

    FunCall

--- ast/function_call_node.h
+++ ast/function_call_node.h
@@ -11,40 +11,40 @@ namespace l22
 
     class function_call_node : public cdk::expression_node
     {
-        std::string _identifier;
+//        std::string _identifier;
         // TODO checkar comments
-//        cdk::expression_node *_expr;
+        cdk::expression_node *_expr; // ponteiro para a funcao
         cdk::sequence_node *_arguments;
 
 
     public:
         // i
-        function_call_node(int lineno, const std::string &identifier) :
-            cdk::expression_node(lineno), _identifier(identifier), /*_expr(nullptr),*/ _arguments(new cdk::sequence_node(lineno)){
-        }
-        // i + 1
-        function_call_node(int lineno, const std::string &identifier, cdk::sequence_node *arguments) :
-            cdk::expression_node(lineno), _identifier(identifier), /*_expr(nullptr),*/ _arguments(arguments){
-        }
+//        function_call_node(int lineno, const std::string &identifier) :
+//            cdk::expression_node(lineno), _identifier(identifier), /*_expr(nullptr),*/ _arguments(new cdk::sequence_node(lineno)){
+//        }
+        // cdi + 1
+//        function_call_node(int lineno, const std::string &identifier, cdk::sequence_node *arguments) :
+//            cdk::expression_node(lineno), _identifier(identifier), /*_expr(nullptr),*/ _arguments(arguments){
+//        }
         // 1
-//        function_call_node(int lineno, cdk::expression_node *expr) :
-//            cdk::expression_node(lineno), _identifier(""), _expr(expr), _arguments(new cdk::sequence_node(lineno)){
-//        }
+        function_call_node(int lineno, cdk::sequence_node *arguments) :
+            cdk::expression_node(lineno), _arguments(arguments){
+        }
         // 1 + 1
-//        function_call_node(int lineno, cdk::expression_node *expr, cdk::sequence_node *arguments) :
-//            cdk::expression_node(lineno), _identifier(""), _expr(expr), _arguments(arguments){
-//        }
+        function_call_node(int lineno, cdk::expression_node *expr, cdk::sequence_node *arguments) :
+            cdk::expression_node(lineno), _expr(expr), _arguments(arguments){
+        }
 
     public:
-        const std::string &identifier(){
-            return _identifier;
-        }
+//        const std::string &identifier(){
+//            return _identifier;
+//        }
         cdk::sequence_node *arguments(){
             return _arguments;
         }
-//        cdk::expression_node *expr(){
-//            return _expr;
-//        }
+        cdk::expression_node *expr(){
+            return _expr;
+        }
 
         void accept(basic_ast_visitor *sp, int level){
             sp->do_function_call_node(this, level);
